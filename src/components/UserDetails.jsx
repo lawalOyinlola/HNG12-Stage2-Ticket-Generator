1a9e06577b62c6ab7521a4815b821a0aeb6a0cd9
Lawal Oyinlola <oyinlolalawal1705@gmail.com>
2025-02-15 14:41:26 +0000

    improve user experience
    added icon to input error, added icon to email input, adeded validation for fullname input,set color for ticket btn

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -73,22 +73,42 @@ function UserDetails({ setStep, setFormData, formData }) {
               Enter your name
             </label>
             <input
-              {...register("fullName", { required: "Full name is required" })}
+              {...register("fullName", {
+                required: "Full name is required",
+                minLength: {
+                  value: 4,
+                  message: "Full name must be at least 4 characters long",
+                },
+              })}
               type="text"
               className={errors.fullName ? "input-error" : ""}
               aria-describedby="name-error"
+              placeholder="John Snow"
             />
             {errors.fullName && (
               <span id="name-error" className="error">
-                {errors.fullName.message}
+                ⚠️ {errors.fullName.message}
               </span>
             )}
           </div>
 
-          <div>
+          <div className="email-container">
             <label htmlFor="name" className="label">
               Enter your email *
             </label>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              className="email-icon"
+            >
+              <path
+                d="M20 4.00012H4C2.897 4.00012 2 4.89712 2 6.00012V18.0001C2 19.1031 2.897 20.0001 4 20.0001H20C21.103 20.0001 22 19.1031 22 18.0001V6.00012C22 4.89712 21.103 4.00012 20 4.00012ZM20 6.00012V6.51112L12 12.7341L4 6.51212V6.00012H20ZM4 18.0001V9.04412L11.386 14.7891C11.5611 14.9267 11.7773 15.0014 12 15.0014C12.2227 15.0014 12.4389 14.9267 12.614 14.7891L20 9.04412L20.002 18.0001H4Z"
+                fill="white"
+              />
+            </svg>
             <input
               {...register("email", {
                 required: "Email is required",
@@ -104,7 +124,7 @@ function UserDetails({ setStep, setFormData, formData }) {
             />
             {errors.email && (
               <span id="email-error" className="error">
-                {errors.email.message}
+                ⚠️ {errors.email.message}
               </span>
             )}
           </div>
@@ -116,6 +136,8 @@ function UserDetails({ setStep, setFormData, formData }) {
             <textarea
               {...register("specialRequest")}
               id="specialRequest"
+              placeholder="Make a special request"
+              rows={3}
             ></textarea>
           </div>
 
